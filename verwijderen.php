ee397eda5802abd1357b78b7e8e82e7cb7c36fae
ramonvloon <ramonvloon@hotmail.com>
2011-07-25 17:39:23 +0200

    bestand verwijderen
    Signed-off-by: ramonvloon <ramonvloon@hotmail.com>

---
+++ verwijderen.php
@@ -0,0 +1,13 @@
+<table border="0" width="100%" height="0">
+<tr>
+<td width="85%" height="0" valign="top">
+<form action"verwijderenbestand.php" method="POST">
+
+<B>Naam van de foto:</b><br>
+<input type="text" maxlength="40" name="naam" class="form" value="" size="26"><br>
+<BR><input type="submit" value="Verwijderen" name="submit">
+
+</form>
+</td>
+</tr>
+</table> 
