5df8fb7e74dcf46e3eafdab0e9eca79d9916f1ab
Darkrulerz <dark_rulerz@hotmail.com>
2011-07-26 23:45:33 +0200

    Verwijderscript, hoppa!! XD

--- verwijderen.php
+++ verwijderen.php
@@ -1,27 +1,82 @@
 <?php
-if(isset($_GET['delete']) && $_GET['delete']=='true') {
-   unlink("fotos/".$_POST['fileToDelete']);
+if (isset($_POST['fileToDelete'])) {
+    unlink("fotos/" . $_POST['fileToDelete']);
 }
-?>
-<?php 
-                $files = "";
-if ($handle = opendir('fotos/')) {  
-    while (false !== ($file = readdir($handle))) 
-	{
-        if ($file != "." && $file != ".." && is_dir('fotos/' . $file))
-		{   
-            if ($handle2 = opendir('fotos/' . $file)) {
+$files = "";
+$categories = "";
+$handle = opendir('fotos/');
+if ($handle) {
+    while (false !== ($file = readdir($handle))) {
+        if ($file != "." && $file != ".." && is_dir('fotos/' . $file)) {
+            $handle2 = opendir('fotos/' . $file);
+            if ($handle2) {
                 while (($file2 = readdir($handle2)) !== false) {
                     if ($file2 != "." && $file2 != ".." && !is_dir($file2)) {
-                                    $files .= "'" . "fotos/" . $file . "/" . $file2 . "',";
-                                    }}}
-        }   
+                        $files .= "fotos/" . $file . "/" . $file2 . ",";
+                    }
+                }
+                $categories .= $file . ",";
+            }
+        }
     }
     closedir($handle);
+    $files = substr($files, 0, strlen($files) - 1);
+    $categories = substr($categories, 0, strlen($categories) - 1);
+}
+$files = explode(',', $files);
+$categories = explode(',', $categories);
+echo '
+    <html>
+        <head>
+            <title>Verwijderscript</title>
+        </head>
+        <body>
+            <form method="GET" action="' . $_SERVER['PHP_SELF'] . '">
+                <select name="category" onChange="this.form.submit();">
+';
+foreach ($categories as $category) {
+    if (isset($_GET['category'])) {
+        if ($category == $_GET['category']) {
+            echo '<option value="' . $category . '" selected>' . $category . '</option>';
+        } else {
+            echo '<option value="' . $category . '">' . $category . '</option>';
+        }
+    } else {
+        echo '<option value="' . $category . '">' . $category . '</option>';
     }
-			echo "<td><a href=".$files."><img src=".$files." width=\"70\" height=\"50\"></a></td>";
+}
+echo '
+                </select>
+            </form>
+            <div style="width:300px;height:300px;">
+                <script type="text/javascript">
+                    function submit(filename) {
+                        if (confirm(\'Are you sure you want to delete\' + filename + \'?\')) {
+                            document.getElementById(\'fileToDelete\').value=filename;
+                            document.getElementById(\'fileDeletionForm\').submit();
+                        }
+                    }
+                </script>
+                <form id="fileDeletionForm" method="POST" action="' . $_SERVER['PHP_SELF'] . '">
+                    <input type="hidden" id="fileToDelete" name="fileToDelete" />
+';
+foreach($files as $file) {
+    if (isset($_GET['category'])) {
+        if (strstr($file, $_GET['category']) != false) {
+            echo '
+                <img src="' . $file . '" alt="' . $file . '" style="width:200px;" onClick="submit(\'' . $file . '\');"/>
+            ';
+        }
+    } else {
+        echo '
+            <img src="' . $file . '" alt="' . $file . '" style="width:200px;" onClick="submit(\'' . $file . '\');"/>
+        ';
+    }
+}
+echo '
+                </form>
+            </div>
+        </body>
+    </html>
+';
 			
-			echo "<form method=\"post\" name=\"deleteSomething\" action="?><?php echo $_SERVER['PHP_SELF'].'?delete=true';?><?php echo" >
-			<input type=\"text\" name=\"fileToDelete\" value=".$files." >
-			<input type=\"submit\" value=\"Delete\"></form>";
-?>
