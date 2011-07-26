4ef1a7e8357bb06932bbe456b04b2b5e3c121985
Darkrulerz <dark_rulerz@hotmail.com>
2011-07-27 01:02:20 +0200

    Waarschijnlijk ga je deze file niet eens gebruiken...maar filter voor de niet-files :)

--- showpictures.php
+++ showpictures.php
@@ -4,7 +4,9 @@ $category = 'fotos/';
 $handle = opendir($category);
 if ($handle) {
     while (($file = readdir($handle)) !== false) {
-        echo '<img style="height:200px; width:250px;" src="' . $category . '/' . $file . '" alt="' . $file . '">';
+        if ($file != '.' && $file != '..' && is_file($category . '/' . $file)) {
+            echo '<img style="height:200px; width:250px;" src="' . $category . '/' . $file . '" alt="' . $file . '">';
+        }
     }
     closedir($handle);
 }
